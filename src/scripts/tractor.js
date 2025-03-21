dae0291225d3bfd01d79bd32e72e940a3df9cd25
Charlie Wunderlich <cww324@gmail.com>
2025-03-21 13:52:34 -0500

    yep it works

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -6,28 +6,25 @@ import { createAsparagus } from './seeds/asparagus.js';
 import { createWheat } from './seeds/wheat.js';
 import { createPotato } from './seeds/potato.js';
 
-export const plantSeeds = (plantingPlan) => { 
-    for (let i = 0; i < plantingPlan.length; i++){
-        const row = plantingPlan[i];
-
-        for (let j = 0; j < row.length; j++) {
-            const seedType = row[j];
-            
-            if (seedType === "Soybean") {
-                addPlant(createSoybean());
-            } else if (seedType === "Corn") {
-                addPlant(createCorn());
-            } else if (seedType === "Sunflower") {
-                addPlant(createSunflower());
-            } else if (seedType === "Asparagus") {
-                addPlant(createAsparagus());
-            } else if (seedType === "Wheat") {
-                addPlant(createWheat());
-            } else if (seedType === "Potato") {
-                addPlant(createPotato());
-            }
-        }
+export const plantSeeds = (plantingPlan) => {
+  // Iterate over each row in the planting plan.
+  for (const row of plantingPlan) {
+    // Iterate over each seed type in the row.
+    for (const seedType of row) {
+      if (seedType === "Soybean") {
+        addPlant(createSoybean());
+      } else if (seedType === "Corn") {
+        addPlant(createCorn());
+      } else if (seedType === "Sunflower") {
+        addPlant(createSunflower());
+      } else if (seedType === "Asparagus") {
+        addPlant(createAsparagus());
+      } else if (seedType === "Wheat") {
+        addPlant(createWheat());
+      } else if (seedType === "Potato") {
+        addPlant(createPotato());
+      }
     }
-};
-
+  }
+}
 
